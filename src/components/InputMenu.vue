725fcd2ab579a1b0e7210f088a603baf2a09e40c
matsushita <m_susumu@apes.jp>
2019-10-08 18:43:14 +0900

    update:ui修正

--- src/components/InputMenu.vue
+++ src/components/InputMenu.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
     <b-card title="昼食メニュー" class="title">
-      name
-      <b-input type="text"></b-input>price
-      <b-input type="number"></b-input>Cal
-      <b-input type="number"></b-input>picture
-      <b-input type="string"></b-input>type
-      <b-input type="string"></b-input>date
-      <b-input type="datetime"></b-input>
+      <div>
+        <p>商品名</p>
+        <b-input type="text"></b-input>
+        <p>価格</p>
+        <b-input type="number"></b-input>
+        <p>カロリー</p>
+        <b-input type="number"></b-input>
+        <p>種別</p>
+        <b-input type="string"></b-input>
+        <p>写真</p>
+        <b-input type="string"></b-input>
+        <p>日付</p>
+        <b-input type="datetime"></b-input>
+      </div>
     </b-card>
   </div>
 </template>
-<script src="https://www.gstatic.com/firebasejs/6.5.0/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/6.5.0/firebase-auth.js"></script>
 <script src="https://www.gstatic.com/firebasejs/6.5.0/firebase-database.js"></script>
 <script src="https://www.gstatic.com/firebasejs/6.5.0/firebase-storage.js"></script>
